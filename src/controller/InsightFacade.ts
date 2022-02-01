e4dcba0ccb6bfc2b3a671e1e2908147db1c32686
xwang90 <github.students.cs.ubc.ca>
2022-01-31 21:44:08 -0800

    c0 tests

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -1,11 +1,4 @@
-import {
-	IInsightFacade,
-	InsightDataset,
-	InsightDatasetKind,
-	InsightError,
-	InsightResult,
-	NotFoundError
-} from "./IInsightFacade";
+import {IInsightFacade, InsightDataset, InsightDatasetKind, InsightError, InsightResult} from "./IInsightFacade";
 
 /**
  * This is the main programmatic entry point for the project.
@@ -13,16 +6,42 @@ import {
  *
  */
 export default class InsightFacade implements IInsightFacade {
+	public datasets: InsightDataset[];
 	constructor() {
 		console.log("InsightFacadeImpl::init()");
+		this.datasets = [];
 	}
 
 	public addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
+		if(id.trim().length === 0) {
+			return Promise.reject(new InsightError("id cannot only be whitespace"));
+		}
+
+		if(kind !== InsightDatasetKind.Courses) {
+			return Promise.reject(new InsightError("invalid dataset kind"));
+		}
+
+		if(id.includes("_")) {
+			return Promise.reject(new InsightError("id cannot contain an underscore"));
+		}
+
+		if(id === "") {
+			return Promise.reject(new InsightError("id cannot be empty"));
+		}
 		return Promise.reject("Not implemented.");
 	}
 
 	public removeDataset(id: string): Promise<string> {
+		if(id.trim().length === 0) {
+			return Promise.reject(new InsightError("id cannot only be whitespace"));
+		}
+
+		if(id.includes("_")) {
+			return Promise.reject(new InsightError("id cannot contain an underscore"));
+		}
 		return Promise.reject("Not implemented.");
+
+
 	}
 
 	public performQuery(query: unknown): Promise<InsightResult[]> {
