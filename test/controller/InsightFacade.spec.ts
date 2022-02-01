d4abd2a6816a622a58fba98ef5122ab647ca4a87
UBC Student <jkristja@students.cs.ubc.ca>
2022-01-31 19:23:26 -0800

    Port of jkristja C0 test suite (#3)
    Co-authored-by: JKristja <75544688+JKristja@users.noreply.github.com>

--- test/controller/InsightFacade.spec.ts
+++ test/controller/InsightFacade.spec.ts
@@ -1,7 +1,7 @@
 import {
 	InsightDatasetKind,
 	InsightError,
-	InsightResult,
+	InsightResult, NotFoundError,
 	ResultTooLargeError
 } from "../../src/controller/IInsightFacade";
 import InsightFacade from "../../src/controller/InsightFacade";
@@ -9,7 +9,10 @@ import InsightFacade from "../../src/controller/InsightFacade";
 import * as fs from "fs-extra";
 
 import {folderTest} from "@ubccpsc310/folder-test";
-import {expect} from "chai";
+import chai, {expect} from "chai";
+import chaiAsPromised from "chai-as-promised";
+
+chai.use(chaiAsPromised);
 
 describe("InsightFacade", function () {
 	let insightFacade: InsightFacade;
@@ -21,6 +24,15 @@ describe("InsightFacade", function () {
 	// automatically be loaded in the 'before' hook.
 	const datasetsToLoad: {[key: string]: string} = {
 		courses: "./test/resources/archives/courses.zip",
+		validOneOne: "./test/resources/archives/single_single_valid.zip",
+		validOneMany: "./test/resources/archives/single_multi_valid.zip",
+		validManyOne: "./test/resources/archives/multi_single_valid.zip",
+		validManyMany: "./test/resources/archives/multi_multi_valid.zip",
+		invalidDirectory: "./test/resources/archives/invalid_directory.zip",
+		invalidExtension: "./test/resources/archives/invalid_extension.7z",
+		invalidFormat: "./test/resources/archives/invalid_format.zip",
+		invalidNoCourses: "./test/resources/archives/invalid_no_courses.zip",
+		invalidNoSections: "./test/resources/archives/invalid_no_sections.zip",
 	};
 
 	before(function () {
@@ -64,6 +76,153 @@ describe("InsightFacade", function () {
 			return insightFacade.addDataset(id, content, InsightDatasetKind.Courses).then((result: string[]) => {
 				expect(result).to.deep.equal(expected);
 			});
+		});
+		it("should contain at least one course with at least one section", function() {
+			let content: string = datasetContents.get("validOneOne") ?? "";
+			return insightFacade.addDataset("single-single", content, InsightDatasetKind.Courses)
+				.then((addedIds) => {
+					expect(addedIds).to.be.an.instanceof(Array);
+					expect(addedIds).to.have.length(1);
+					expect(addedIds.includes("single-single"));
+				});
+
+			content = datasetContents.get("validOneMany") ?? "";
+			return insightFacade.addDataset("single-multi", content, InsightDatasetKind.Courses)
+				.then((addedIds) => {
+					expect(addedIds).to.be.an.instanceof(Array);
+					expect(addedIds).to.have.length(2);
+					expect(addedIds.includes("single-multi"));
+				});
+
+			content = datasetContents.get("validManyOne") ?? "";
+			return insightFacade.addDataset("multi-1", content, InsightDatasetKind.Courses)
+				.then((addedIds) => {
+					expect(addedIds).to.be.an.instanceof(Array);
+					expect(addedIds).to.have.length(3);
+					expect(addedIds.includes("multi-1"));
+				});
+
+			content = datasetContents.get("validManyMany") ?? "";
+			return insightFacade.addDataset("multi-2", content, InsightDatasetKind.Courses)
+				.then((addedIds) => {
+					expect(addedIds).to.be.an.instanceof(Array);
+					expect(addedIds).to.have.length(4);
+					expect(addedIds.includes("multi-2"));
+				});
+		});
+		it("should reject if not a .zip file", function() {
+			const content: string = datasetContents.get("invalidExtension") ?? "";
+			return expect(insightFacade.addDataset("7z extension", content, InsightDatasetKind.Courses))
+				.eventually.to.be.rejectedWith(InsightError);
+		});
+
+		it("should reject if not contain courses/", function () {
+			const content: string = datasetContents.get("invalidDirectory") ?? "";
+			return expect(insightFacade.addDataset("no courses/ dir", content, InsightDatasetKind.Courses))
+				.eventually.to.be.rejectedWith(InsightError);
+		});
+
+		it("should reject non-JSON format", function () {
+			const content: string = datasetContents.get("invalidFormat") ?? "";
+			return expect(insightFacade.addDataset("not JSON form", content, InsightDatasetKind.Courses))
+				.eventually.to.be.rejectedWith(InsightError);
+		});
+
+		it("should reject if only courses with zero sections", function () {
+			const content: string = datasetContents.get("invalidNoSections") ?? "";
+			return expect(insightFacade.addDataset("zero sections", content, InsightDatasetKind.Courses))
+				.eventually.to.be.rejectedWith(InsightError);
+		});
+
+		it("should reject an invalid id", function () {
+			const content: string = datasetContents.get("courses") ?? "";
+			return expect(insightFacade.addDataset("no_underscores", content, InsightDatasetKind.Courses))
+				.eventually.to.be.rejectedWith(InsightError);
+			return expect(insightFacade.addDataset("   ", content, InsightDatasetKind.Courses))
+				.eventually.to.be.rejectedWith(InsightError);
+			return expect(insightFacade.addDataset("", content, InsightDatasetKind.Courses))
+				.eventually.to.be.rejectedWith(InsightError);
+		});
+
+		it("should reject a duplicate id", function () {
+			let content: string = datasetContents.get("validOneOne") ?? "";
+			return insightFacade.addDataset("no duplicates", content, InsightDatasetKind.Courses)
+				.then((addedIds) => {
+					expect(addedIds).to.be.an.instanceof(Array);
+					expect(addedIds).to.have.length(1);
+					expect(addedIds.includes("no duplicates"));
+				});
+			content = datasetContents.get("courses") ?? "";
+			return expect(insightFacade.addDataset("no duplicates", content, InsightDatasetKind.Courses))
+				.eventually.to.be.rejectedWith(InsightError);
+		});
+		it("should remove the Dataset with and return the matching id", function() {
+			const content: string = datasetContents.get("courses") ?? "";
+			return insightFacade.addDataset("courses", content, InsightDatasetKind.Courses);
+			return insightFacade.removeDataset("courses").then((removedId) => {
+				expect(removedId).to.deep.equal("courses");
+			});
+			return insightFacade.listDatasets().then((insightDatasets) => {
+				expect(insightDatasets).to.have.length(0);
+			});
+		});
+
+		it("should reject removal of non-existent set with NotFoundError", function() {
+			return expect(insightFacade.removeDataset("courses")).eventually.to.be.rejectedWith(NotFoundError);
+		});
+
+		it("should reject an invalid id with InsightError", function() {
+			return expect(insightFacade.removeDataset("no_underscores")).eventually.to.be.rejectedWith(InsightError);
+			return expect(insightFacade.removeDataset("      ")).eventually.to.be.rejectedWith(InsightError);
+			return expect(insightFacade.removeDataset("")).eventually.to.be.rejectedWith(InsightError);
+		});
+		it("should list no datasets", function() {
+			return insightFacade.listDatasets().then((insightDatasets) => {
+				expect(insightDatasets).to.be.an.instanceof(Array);
+				expect(insightDatasets).to.have.length(0);
+			});
+		});
+
+		it("should list one dataset", function() {
+			const content: string = datasetContents.get("courses") ?? "";
+			return insightFacade.addDataset("courses", content, InsightDatasetKind.Courses)
+				.then((addedIds) => insightFacade.listDatasets())
+				.then((insightDatasets) => {
+					expect(insightDatasets).to.deep.equal( [{
+						id: "courses",
+						kind: InsightDatasetKind.Courses,
+						numRows: 64612,
+					}]);
+				});
+		});
+
+		it("should list multiple datasets", function() {
+			const content: string = datasetContents.get("courses") ?? "";
+			return insightFacade.addDataset("courses", content, InsightDatasetKind.Courses)
+				.then(() => {
+					return insightFacade.addDataset("courses-2", content, InsightDatasetKind.Courses);
+				})
+				.then(() => {
+					return insightFacade.listDatasets();
+				})
+				.then((insightDatasets) => {
+					expect(insightDatasets).to.be.an.instanceof(Array);
+					expect(insightDatasets).to.have.length(2);
+					let insightDatasetCourses = insightDatasets.find((dataset) => dataset.id === "courses");
+					expect(insightDatasetCourses).to.exist;
+					expect(insightDatasetCourses).to.deep.equal({
+						id: "courses",
+						kind: InsightDatasetKind.Courses,
+						numRows: 64612,
+					});
+					insightDatasetCourses = insightDatasets.find((dataset) => dataset.id === "courses-2");
+					expect(insightDatasetCourses).to.exist;
+					expect(insightDatasetCourses).to.deep.equal({
+						id: "courses-2",
+						kind: InsightDatasetKind.Courses,
+						numRows: 64612,
+					});
+				});
 		});
 	});
 
