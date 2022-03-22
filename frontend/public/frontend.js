593263b6b08a265d9137564629abb96d4fbdd728
JKristja <75544688+JKristja@users.noreply.github.com>
2022-03-22 11:25:51 -0700

    Updated Server.ts to handle PerformQuery; added basic client page for User Story 2;  Partially implemented User Story 2.

--- frontend/public/frontend.js
+++ frontend/public/frontend.js
@@ -1,5 +1,57 @@
 document.getElementById("click-me-button").addEventListener("click", handleClickMe);
+document.getElementById("findRoomButton").addEventListener("click", findRooms);
 
 function handleClickMe() {
 	alert("Button Clicked!");
+}
+
+function findRooms() {
+
+	let furn_type = document.getElementById("furn_type").value
+	let min_seats = document.getElementById("room_cap").value
+
+	if (min_seats.match(/^[0-9]+$/)) {
+		min_seats = parseInt(min_seats)
+	}
+
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", "http://localhost:4321/query");
+
+	xhr.setRequestHeader("Content-Type", "application/json");
+
+	let jsonQuery = {
+			WHERE: {
+				AND: [
+					{
+						IS: {
+							"rooms_furniture": furn_type
+						}
+					},
+					{
+						NOT: {
+							LT: {
+								"rooms_seats": min_seats
+							}
+						}
+					}
+					]
+			},
+			OPTIONS: {
+				COLUMNS: [
+					"rooms_name",
+					"rooms_seats",
+					"rooms_type",
+					"rooms_furniture"
+				],
+				ORDER: "rooms_seats"
+			}
+		}
+
+	xhr.send(JSON.stringify(jsonQuery));
+
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState === 4) {
+			document.getElementById("roomsOut").innerHTML = JSON.parse(xhr.responseText);
+			alert(xhr.responseText);
+		}};
 }
