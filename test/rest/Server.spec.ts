111239b5dee37bf48bf4c99cb8827362a5693afe
UBC Student <xwang90@students.cs.ubc.ca>
2022-03-28 14:25:09 -0700

    C3 phase2 (#42)
    * Add room dataset
    
    * C2
    
    * C2
    
    * fixed getbuilding function
    
    * fixed get buildings
    
    * fixed parseRooms
    
    * c2
    
    * c2
    
    * fixed c2
    
    * fixed no-await-in-loop
    
    * c3 server
    
    * refactored c2
    
    * Updated Server.ts to handle PerformQuery; added basic client page for User Story 2;  Partially implemented User Story 2.
    
    * c3 user story 1
    
    * Update RoomHelper.ts
    
    * c3
    
    * c3
    
    Co-authored-by: xwang90 <github.students.cs.ubc.ca>
    Co-authored-by: JKristja <75544688+JKristja@users.noreply.github.com>

--- test/rest/Server.spec.ts
+++ test/rest/Server.spec.ts
@@ -2,6 +2,7 @@ import Server from "../../src/rest/Server";
 import InsightFacade from "../../src/controller/InsightFacade";
 import {expect, use} from "chai";
 import chaiHttp from "chai-http";
+import * as fs from "fs";
 
 describe("Facade D3", function () {
 
@@ -32,25 +33,24 @@ describe("Facade D3", function () {
 
 	// Sample on how to format PUT requests
 
-	// it("PUT test for courses dataset", function () {
-	// 	try {
-	// 		return chai.request(SERVER_URL)
-	// 			.put(ENDPOINT_URL)
-	// 			.send(ZIP_FILE_DATA)
-	// 			.set("Content-Type", "application/x-zip-compressed")
-	// 			.then(function (res: ChaiHttp.Response) {
-	// 				console.log(res);
-	// 				expect(res.status).to.be.equal(200);
-	// 			})
-	// 			.catch(function (err) {
-	// 				console.log(err);
-	// 				expect.fail();
-	// 			});
-	// 	} catch (err) {
-	// 		console.log(err);
-	// 	}
-	// });
-
+	it("PUT test for courses dataset", function () {
+		try {
+			return chai.request("http://localhost:4321")
+				.put("/dataset/courses/courses")
+				.send("./test/resources/archives/courses.zip")
+				.set("Content-Type", "application/x-zip-compressed")
+				.then(function (res: ChaiHttp.Response) {
+					console.log(res);
+					expect(res.status).to.be.equal(200);
+				})
+				.catch(function (err) {
+					console.log(err);
+					expect.fail();
+				});
+		} catch (err) {
+			console.log(err);
+		}
+	});
 
 	// The other endpoints work similarly. You should be able to find all instructions at the chai-http documentation
 });
